474b492696bd6247f1b4cd5aa66f983f3a2c41c4
lladhibhutall <debjyoti.biswas@outlook.com>
2019-01-03 10:13:39 +0530

    Working on own page but not on wikipedia: maybe something wrong with url

--- content.js
+++ content.js
@@ -5,16 +5,18 @@ let paragraphs = document.getElementsByTagName('p');
 //console.log(url);
 const url1 = chrome.extension.getURL('Dotsies.ttf');
 console.log(url1);
-var junction_font = new FontFace('JunctionRegular','url(chrome-extension://dkpeplkicbicohokcccgidhagbpndpnj/Dotsies.ttf)');
+var junction_font = new FontFace('JunctionRegular','url(chrome-extension://jnohippkdnjbmndieeboeiknopapiifh/Dotsies.ttf)');
 for(ele of paragraphs){
+
+ele.innerHTML = "Say What";
 
 junction_font.load().then(function(loaded_face) {
 document.fonts.add(loaded_face);
 console.log('Fuck it');
-ele.innerHTML="My name";
+ele.innerHTML="My name is slim shady";
 //document.querySelector("#demo").innerHTML = "My name is slime shady";
 //document.querySelector("#demo").style.fontFamily = '"Junction Regular", Tahoma';
-ele.style.fontFamily = '"JunctionRegular"';
+ele.style.fontFamily = 'JunctionRegular';
 
 
 }).catch(function(error) {
